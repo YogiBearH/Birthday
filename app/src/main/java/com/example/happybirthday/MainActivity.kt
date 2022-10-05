77585ae47ab57439d695a9dad3f472c0d44f09b5
John Nguyen <jnguyen@catalystsolves.com>
2022-10-05 11:01:50 -0400

    Added image with text

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.happybirthday
 
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +38,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthdayGreetingWithText(message1: String, message2: String, from: String) {
-    Surface(color = Color.DarkGray) {
+    Surface(color = Color.Transparent) {
         Column(modifier = Modifier.padding(100.dp)) {
             Text(
                 text = message1,
@@ -61,10 +63,15 @@ fun BirthdayGreetingWithText(message1: String, message2: String, from: String) {
 @Composable
 fun BirthdayGreetingWithImage(message1: String, message2: String, from: String) {
     val image = painterResource(R.drawable.androidparty)
-    Image(
-        painter = image,
-        contentDescription = null
-    )
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message1 = message1, message2 = message2, from = from)
+    }
 }
 
 @Preview(showBackground = true)
